da8421d967632549b8b20effbc77ef1e59c63d6f
bbyliu <lisakimimi@gmail.com>
2022-09-23 20:01:49 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+Console.WriteLine("1. Угадай число");
+Console.WriteLine("2. Таблица умножения");
+Console.WriteLine("3. Вывод делителй числа");
+Console.WriteLine("4. Выйти из программы");
+
+while (true)
+{
+    char numb;
+
+    Console.WriteLine("Выберите операцию: ");
+
+
+    numb = Convert.ToChar(Console.ReadLine());
+    if (numb == '4')
+    {
+        Environment.Exit(0);
+    }
+
+    if (numb == '1')
+    {
+        Console.WriteLine("Угадай число от 0 до 100");
+        Random rnd = new Random();
+        int guess = rnd.Next(1, 100);
+
+        for(int i = 0; i < 100; i++)
+        {
+            int enter = Convert.ToInt32(Console.ReadLine());
+            if (enter < guess)
+            {
+                Console.WriteLine("Нужно больше");
+            }
+            if (enter > guess)
+            {
+                Console.WriteLine("Нужно меньше");
+            }
+            else if (enter == guess)
+            {
+                Console.WriteLine("Угадал!");
+                break;
+            }
+        }
+    }
+
+    if (numb == '2')
+    {
+        int[,] tab = new int[10, 10];
+
+        for(int i = 1; i < tab.GetLength(0); i++)
+        {
+            for (int j = 1; j < tab.GetLength(1); j++)
+            {
+                tab[i, j] = i * j;
+            }
+        }
+        for(int i = 1; i < tab.GetLength(0); i++)
+        {
+            Console.WriteLine();
+            for (int j = 1; j < tab.GetLength(1); j++)
+            {
+                Console.Write("{0,3}", tab[i, j]);
+            }
+        }
+        Console.WriteLine();
+    }
+
+    if (numb == '3')
+    {
+        bool d = true;
+        while (d)
+        {
+            int a;
+            Console.WriteLine("Введите число: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            if (true)
+            {
+                for (int i = 1; i <= a; i++)
+                {
+                    if (a % i == 0)
+                    {
+                        Console.Write(i + "\t");
+                    }
+
+                }
+                Console.WriteLine();
+                Console.WriteLine("Чтобы выйти напиши 'выход'");
+            }
+            if(Console.ReadLine().Equals("выход"))
+            {
+                d = false;
+            }
+        }
+
+    }  
+
+}
+
